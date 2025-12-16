82d5ab45045d69c3d77839bfe25f3c8bb00d3adc
BigWorm7 <maciej.ziaja05@o2.pl>
2025-12-16 11:01:20 +0100

    dodawanie

--- app/src/main/java/com/pt2/kosz/MainActivity.java
+++ app/src/main/java/com/pt2/kosz/MainActivity.java
@@ -1,24 +1,50 @@
 package com.pt2.kosz;
 
 import android.os.Bundle;
+import android.view.View;
 
+import androidx.activity.ActivityFlags;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.pt2.kosz.databinding.ActivityMainBinding;
 
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+    int punkty = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        //binding.textView2.setText("100");
+        binding.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                punkty++;
+                binding.textView2.setText(""+punkty);
+            }
         });
+        binding.button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                punkty+=2;
+                binding.textView2.setText(""+punkty);
+            }
+        });
+        binding.button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                punkty+=3;
+                binding.textView2.setText(""+punkty);
+            }
+        });
+
     }
 }
